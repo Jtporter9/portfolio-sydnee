6cb9d03b177e5c95503c447d310442e1a100859f
J. Tanner Porter <jtporter9@gmail.com>
2016-02-16 15:06:28 -0700

    working on admin

--- Public/js/admin/admin.js
+++ Public/js/admin/admin.js
@@ -1,6 +1,13 @@
 angular.module('portfolio').controller('adminCtrl', function ($scope, mainService) {
 
   $scope.intro = mainService.intro;
-
+  $scope.headShot = mainService.headShot;
+  $scope.projects = mainService.projects;
+  $scope.bioTitle = mainService.bioTitle;
+  $scope.p1 = mainService.p1;
+  $scope.p2 = mainService.p2;
+  $scope.p3 = mainService.p3;
+  $scope.p4 = mainService.p4;
+  $scope.p5 = mainService.p5;
 
 });
--- Public/js/landing/landing.js
+++ Public/js/landing/landing.js
@@ -1,5 +1,12 @@
-angular.module('portfolio').controller('landingCtrl', function ($scope, mainService) {
+angular.module('portfolio').controller('landingCtrl', function($scope, mainService) {
 
-$scope.intro = mainService.intro;
-
+  $scope.headShot = mainService.headShot;
+  $scope.intro = mainService.intro;
+  $scope.projects = mainService.projects;
+  $scope.bioTitle = mainService.bioTitle;
+  $scope.p1 = mainService.p1;
+  $scope.p2 = mainService.p2;
+  $scope.p3 = mainService.p3;
+  $scope.p4 = mainService.p4;
+  $scope.p5 = mainService.p5;
 });
