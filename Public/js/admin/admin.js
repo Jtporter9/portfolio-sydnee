cf54599ec132d52e5a1f610a8b083e2ac7d88b73
J. Tanner Porter <jtporter9@gmail.com>
2016-02-16 17:00:09 -0700

    admin side looking good

--- Public/js/admin/admin.js
+++ Public/js/admin/admin.js
@@ -1,4 +1,4 @@
-angular.module('portfolio').controller('adminCtrl', function ($scope, mainService) {
+angular.module('portfolio').controller('adminCtrl', function($scope, mainService) {
 
   $scope.intro = mainService.intro;
   $scope.headShot = mainService.headShot;
@@ -10,4 +10,17 @@ angular.module('portfolio').controller('adminCtrl', function ($scope, mainServic
   $scope.p4 = mainService.p4;
   $scope.p5 = mainService.p5;
 
+  $scope.showTrash = function() {
+    $scope.showTrashCan = !$scope.showTrashCan;
+  };
+  
+  $scope.showAddProjBox = function() {
+    $scope.showAddProj = !$scope.showAddProj;
+  };
+
+  $scope.addNewProj = function() {
+    $scope.newProj.number = $scope.projects.length + 1;
+    mainService.projects.push($scope.newProj);
+    $scope.showAddProjBox();
+  };
 });
